9b351e86804e86fee844e0b63cc00d02700923df
鈴木ちほり <suzukichihori@suzukichihoris-MacBook-Air.local>
2020-10-05 11:13:59 -0700

    first commit

---
+++ MyFirstJavaProgram.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class MyFirstJavaProgram {
+    public static void main(String[] args){
+        int num = 123;
+        System.out.println(num);
+        num = 144;
+        System.out.println(num);
+    }
+}
